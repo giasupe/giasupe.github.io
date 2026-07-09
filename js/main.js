e9cddd938c22f54646aac87029adb131e216d94c
giasupe <giasupe.com@gmail.com>
2026-07-09 18:40:52 +0900

    feature/copy_email

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-const link = document.getElementById('copy-email');
+const textElement = document.getElementById('copy-email');
+
+function resetText() {
+    document.getElementById('copy-email').textContent = 'Email';
+}
+
+function copyEmail() {
+    const email = 'giasupe.com@gmail.com';
+    navigator.clipboard.writeText(email);
+    document.getElementById('copy-email').textContent = 'Copied!';
+    setTimeout(resetText, 1500);
+}
+textElement.addEventListener('click', copyEmail);
